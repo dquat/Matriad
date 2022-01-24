4b4d9b4825a6738a0e609729b80884d9a1d249c3
dquat <user.some1@outlook.com>
2022-01-24 22:04:29 +0530

    compiles now

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,6 @@ fn main() {
     let src = "\ntada bada\n\n\nbooom boom mooob\n soosunsuen nshhe dje jeiei";
     let rng = Span::from_range(11..22);
     // let mut exclude = Vec::new();
-    vec![].collect::
     let mut vis = Visualizer::new_span(src, rng, "Main visualizer");
     dbg!(&vis);
     println!("{vis}");
